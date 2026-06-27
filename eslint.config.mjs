2d323b25c4c99805528c1a43c9d98dadcb640c4e
Petrovskyi <ivan.prog74@gmail.com>
2026-06-27 18:07:31 +0200

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,17 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+    rules: {
+      semi: 'error',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'no-undef': 'error',
+    },
+  },
 ]);
