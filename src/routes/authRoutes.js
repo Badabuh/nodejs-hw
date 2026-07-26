df99a7634fd5a2c8fc493b8774a70880f648590b
Petrovskyi <ivan.prog74@gmail.com>
2026-07-26 21:10:51 +0200

    fix

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,15 +3,15 @@ import { celebrate } from 'celebrate';
 import {
   createUserSchema,
   loginUserSchema,
-  resetPasswordSchema,
-  resetEmailSchema,
+  // resetPasswordSchema,
+  // resetEmailSchema,
   getSessionSchema
 } from '../validations/authValidate.js';
 import {
   createUser,
   loginUser,
-  resetPassword,
-  resetEmail,
+  // resetPassword,
+  // resetEmail,
   logoutUser,
   refreshSession
 } from '../controllers/authControllers.js';
@@ -20,8 +20,8 @@ const router = Router();
 
 router.post('/users/register', celebrate(createUserSchema), createUser);
 router.post('/users/login', celebrate(loginUserSchema), loginUser);
-router.post('/users/reset-password', celebrate(resetPasswordSchema), resetPassword);
-router.post('/users/reset-email', celebrate(resetEmailSchema), resetEmail);
+// router.post('/users/reset-password', celebrate(resetPasswordSchema), resetPassword);
+// router.post('/users/reset-email', celebrate(resetEmailSchema), resetEmail);
 router.post('/users/logout', celebrate(getSessionSchema), logoutUser);
 router.get('/auth/session', celebrate(getSessionSchema), refreshSession);
 
