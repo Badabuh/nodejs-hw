af5891aa1f63a38a38d07d153550fff8d115aba0
Petrovskyi <ivan.prog74@gmail.com>
2026-08-02 10:03:52 +0200

    fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,6 +5,7 @@ import { User } from '../models/user.js';
 const updateUserAvatar = async (req, res) => {
   const user = req.user;
   const avatar = req.file;
+  let result;
 
   if (!user) {
     throw createHttpError(401, 'User not authenticated');
@@ -15,22 +16,23 @@ const updateUserAvatar = async (req, res) => {
   }
 
   try {
-    const result = await saveFileToCloudinary(avatar.buffer, user._id);
-    const updatedUser = await User.findByIdAndUpdate(
-      user._id,
-      { avatar: result.secure_url },
-      { returnDocument: 'after' }
-    );
-
-    if (!updatedUser) {
-      throw createHttpError(404, 'User not found');
-    }
-
-    res.status(200).json(updatedUser);
+    result = await saveFileToCloudinary(avatar.buffer, user._id);
   } catch (error) {
     console.error('Error uploading file to Cloudinary:', error);
     throw createHttpError(500, 'Error uploading file');
   }
+
+  const updatedUser = await User.findByIdAndUpdate(
+    user._id,
+    { avatar: result.secure_url },
+    { returnDocument: 'after' }
+  );
+
+  if (!updatedUser) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  res.status(200).json({ url: updatedUser.avatar });
 };
 
 export { updateUserAvatar };
